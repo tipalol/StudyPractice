4843d00281830e0ab295b999c89a33a28dea3dd3
sorokindmitrij <tipalol2@gmail.com>
2019-04-14 21:05:50 +0500

    It works!

--- PracticeFirst/Program.cs
+++ PracticeFirst/Program.cs
@@ -1,77 +1,57 @@
 ﻿using System.IO;
 using System;
-
 namespace PracticeFirst
 {
     class MainClass
     {
-
         public static void Main(string[] args)
         {
-            var reader = new StreamReader("INPUT.TXT"); //Поток ввода
-            var writer = new StreamWriter("OUTPUT.TXT");//Поток вывода
-            var monthSize = new int[] { 0, 31, 0, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 }; 
-            string[] currentDate; //Текущая дата
-            string[] birthdayDate;//Дата дня рождения
-            var year = 0;         //Год рождения 
-            var currentDay = 0;   //Текущий день
-            var currentMonth = 0; //Текущий месяц
-            var birthdayDay = 0;  //День рождения
-            var birthdayMonth = 0;//Месяц рождения
-            var days = 0;         //Дни до следующего дня рождения
-            var monthDays = 0;
-
-            try {
-                currentDate = reader.ReadLine().Split(' ');
-                birthdayDate = reader.ReadLine().Split(' ');
-                reader.Close();
-                currentDay = Convert.ToInt32(currentDate[0]);
-                if (currentDate[1].Contains("0"))
-                    currentMonth = Convert.ToInt32(currentDate[1].Remove(0, 1));
-                else
-                    currentMonth = Convert.ToInt32(currentDate[1]);
-                birthdayDay = Convert.ToInt32(birthdayDate[0]);
-                if (birthdayDate[1].Contains("0"))
-                    birthdayMonth = Convert.ToInt32(birthdayDate[1].Remove(0, 1));
-                else
-                    birthdayMonth = Convert.ToInt32(birthdayDate[1]);
-                year = Convert.ToInt32(birthdayDate[2]);
-            } catch {
-                writer.WriteLine("Ошибка во входных данных");
-                writer.Close();
-                Environment.Exit(1);
-            }
-
-
-
-            //Проверка на високосный год
-            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-                monthSize[2] = 29;
-            else
-                monthSize[2] = 28;
-
-            monthDays = monthSize[currentMonth];
-
-            while (!(currentDay == birthdayDay && currentMonth == birthdayMonth))
+            var reader = new StreamReader("INPUT.TXT");
+            var writer = new StreamWriter("OUTPUT.TXT");
+            var monthSize = new int[] { 0, 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string[] input = reader.ReadLine().Split(' ');
+            int bday = Convert.ToInt32( input[0] );
+            int bmonth = Convert.ToInt32(input[1].Trim('0'));
+            input = reader.ReadLine().Split(' ');
+            reader.Close();
+            int day = Convert.ToInt32(input[0]);
+            int month = Convert.ToInt32(input[1].Trim('0'));
+            int year = Convert.ToInt32(input[2]);
+            int days = 0;
+            while (!(day == bday && month == bmonth))
             {
-                if (currentDay == 31 && currentMonth == 12)
+                int daysInFebruary;
+                if ((year % 4 ==0 && year % 100 != 0) || year % 400 == 0)
                 {
-                    currentDay = 1;
-                    currentMonth = 1;
-                }
-                else if (currentDay == monthDays)
+                    daysInFebruary = 29;
+                } else
                 {
-                    currentDay = 1;
+                    daysInFebruary = 28;
                 }
-                else
-                    days++;
-
+                int daysInMonth = 0;
+                if (month == 2)
+                {
+                    daysInMonth = daysInFebruary;
+                } else
+                {
+                    daysInMonth = monthSize[month];
+                }
+                if (day == 31 && month == 12)
+                {
+                    day = 1;
+                    month = 1;
+                    year++;
+                } else if (day == daysInMonth)
+                {
+                    day = 1;
+                    month++;
+                } else
+                {
+                    day++;
+                }
                 days++;
-
             }
-
-
-            writer.WriteLine(days);
+            writer.Write(days);
             writer.Close();
         }
     }
